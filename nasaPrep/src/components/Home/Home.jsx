5a92b4a858a127dc51c61dc146f0ca754716e66f
kirmitatom <125992097+kirmitatom@users.noreply.github.com>
2025-10-03 22:10:25 +0300

    sixed some styling in the home page

--- nasaPrep/src/components/Home/Home.jsx
+++ nasaPrep/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Home.css";
 import MapComponent from "../../components/Map/Map";
 import HourlyChart from "../../components/HourlyChart/HourlyChart";
 import SpiderWeatherFetcher from '../../components/SpiderWeatherFetcher/SpiderWeatherFetcher';
@@ -7,8 +8,10 @@ const Home = () => {
   return (
     <div>
         <div className="wrapper">
+          <div className="HorizantalWrapper">
             <MapComponent />
             <SpiderWeatherFetcher />
+          </div>
             <HourlyChart />
         </div>
         
