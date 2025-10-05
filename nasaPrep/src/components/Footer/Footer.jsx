8c65b4aa864bdfe0eede6856f47d74154de1892f
Ali Jaffal <ajaffal126@gmail.com>
2025-10-05 16:49:46 +0300

    Refactor Footer component to enable smooth scrolling on route changes

--- nasaPrep/src/components/Footer/Footer.jsx
+++ nasaPrep/src/components/Footer/Footer.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Footer.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Footer() {
+  const location = useLocation();
+
+  // Scroll smoothly to top whenever route changes
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+
   return (
     <div className="footer">
-      
       <ul className="footer-links">
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Contact">Contact</Link></li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
       </ul>
-      
+
       <div className="footer-copyright">
         <hr />
-        <p>Copyright @ 2025 - ALL RIGHTS RESERVED</p>
+        <p>Copyright © 2025 - ALL RIGHTS RESERVED</p>
       </div>
     </div>
   );
